09eccbec1daf489227e5c0bfc57a33c3a112e5d8
Realllyk <1120527990@qq.com>
2022-04-07 15:44:35 +0800

    Camera Movement

--- 421/Assets/Scripts/RoomGenerator.cs
+++ 421/Assets/Scripts/RoomGenerator.cs
@@ -53,7 +53,7 @@ public class RoomGenerator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.anyKeyDown)
+        if(Input.GetKeyDown("`"))
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
